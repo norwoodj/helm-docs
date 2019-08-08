880d39884469b2c02f034908be753c9607650564
Scott Crooks <scott.crooks@gmail.com>
2019-08-08 14:01:27 +0200

    No debug

--- scripts/helm_docs.sh
+++ scripts/helm_docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 if ! command -v helm-docs > /dev/null 2>&1; then
     echo "Please install helm-docs to run the pre-commit hook! https://github.com/norwoodj/helm-docs#installation"
