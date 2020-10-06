445a511bd67e0a828fea32d2339d097006231575
John Norwood <jnorwood@cloudflare.com>
2020-10-06 23:32:42 +0000

    fix: fixes tests by calling correct method

--- pkg/document/model.go
+++ pkg/document/model.go
@@ -28,30 +28,18 @@ type chartTemplateData struct {
 	Values          []valueRow
 }
 
-func getChartTemplateData(chartDocumentationInfo helm.ChartDocumentationInfo, helmDocsVersion string) (chartTemplateData, error) {
-	// handle empty values file case
-	if chartDocumentationInfo.ChartValues.Kind == 0 {
-		return chartTemplateData{
-			ChartDocumentationInfo: chartDocumentationInfo,
-			HelmDocsVersion:        helmDocsVersion,
-			Values:                 make([]valueRow, 0),
-		}, nil
-	}
-
-	if chartDocumentationInfo.ChartValues.Kind != yaml.DocumentNode {
-		return chartTemplateData{}, fmt.Errorf("invalid node kind supplied: %d", chartDocumentationInfo.ChartValues.Kind)
-	}
-	if chartDocumentationInfo.ChartValues.Content[0].Kind != yaml.MappingNode {
-		return chartTemplateData{}, fmt.Errorf("values file must resolve to a map, not %s", strconv.Itoa(int(chartDocumentationInfo.ChartValues.Kind)))
-	}
-
+func getSortedValuesTableRows(documentRoot *yaml.Node, chartValuesDescriptions map[string]helm.ChartValueDescription) ([]valueRow, error) {
 	valuesTableRows, err := createValueRowsFromField(
 		"",
 		nil,
-		chartDocumentationInfo.ChartValues.Content[0],
-		chartDocumentationInfo.ChartValuesDescriptions,
+		documentRoot,
+		chartValuesDescriptions,
 		true,
 	)
+
+	if err != nil {
+		return nil, err
+	}
 
 	sortOrder := viper.GetString("sort-values-order")
 	if sortOrder == FileSortOrder {
@@ -72,6 +60,29 @@ func getChartTemplateData(chartDocumentationInfo helm.ChartDocumentationInfo, he
 			return valuesTableRows[i].Key < valuesTableRows[j].Key
 		})
 	}
+
+	return valuesTableRows, nil
+}
+
+
+func getChartTemplateData(chartDocumentationInfo helm.ChartDocumentationInfo, helmDocsVersion string) (chartTemplateData, error) {
+	// handle empty values file case
+	if chartDocumentationInfo.ChartValues.Kind == 0 {
+		return chartTemplateData{
+			ChartDocumentationInfo: chartDocumentationInfo,
+			HelmDocsVersion:        helmDocsVersion,
+			Values:                 make([]valueRow, 0),
+		}, nil
+	}
+
+	if chartDocumentationInfo.ChartValues.Kind != yaml.DocumentNode {
+		return chartTemplateData{}, fmt.Errorf("invalid node kind supplied: %d", chartDocumentationInfo.ChartValues.Kind)
+	}
+	if chartDocumentationInfo.ChartValues.Content[0].Kind != yaml.MappingNode {
+		return chartTemplateData{}, fmt.Errorf("values file must resolve to a map, not %s", strconv.Itoa(int(chartDocumentationInfo.ChartValues.Kind)))
+	}
+
+	valuesTableRows, err := getSortedValuesTableRows(chartDocumentationInfo.ChartValues.Content[0], chartDocumentationInfo.ChartValuesDescriptions)
 
 	if err != nil {
 		return chartTemplateData{}, err
