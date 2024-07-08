5a5d142c452f9e2ae45d7923c4f7c7bb7be53f3c
John Norwood <norwood.john.m@gmail.com>
2024-07-08 11:11:53 +0100

    fix: Adds check for tag format to release script, also makes release script error on improper tags

--- release.sh
+++ release.sh
@@ -8,6 +8,11 @@ function main {
         exit 1
     fi
 
+    if ! [[ "${release_version}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "Release version must be of the form 'v1.2.3'"
+        exit 1
+    fi
+
     git commit -am "Release ${release_version}" --allow-empty
     git cliff --tag "${release_version}" -o
     git commit --amend -a
